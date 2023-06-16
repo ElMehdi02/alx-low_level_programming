17f4e9b813efeed809286d9052e532178ec2145e
ElMehdi02 <bensoudamehdi6@gmail.com>
2023-06-16 13:02:06 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stidio.h>
+
+/**
+ * main - prints the lowercase alphabet in verse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 122;
+	while (i> 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
