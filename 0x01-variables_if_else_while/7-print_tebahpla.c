0f64e9085b3443a665a1185319302ba4811b958b
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-06-16 21:06:03 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,16 @@
-#include <stidio.h>
+#include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in verse,
+ * main - prints the lowercase alphabet in reverse,
  * followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
 
 	i = 122;
-	while (i> 96)
+	while (i > 96)
 	{
 		putchar(i);
 		i--;
