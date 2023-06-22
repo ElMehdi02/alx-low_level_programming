0f1a04d7fe7484cb07303ab345b31b017003541a
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-06-22 18:07:58 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* _most_numbers - prints numbers
-* description: prints number starting from 0 to 9 except for 2 and 4
-*/
+ * print_most_numbers - prints number
+ * description: prints number starting from 0 to 9 except for 2 and 4
+ */
 
 void print_most_numbers(void)
 {
-	char bumber;
+	char number;
 
 	number = '0';
 	while (number <= '9')
