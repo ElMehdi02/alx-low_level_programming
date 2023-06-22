93523bff4ebbc01b088f4d8669ff34d90ac90b95
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-06-22 14:56:03 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* mul - function that multiplies two numbers
+* mul - function that multiplies a and b
 * @a: first number
 * @b: second number
 * Return: the result of a * b
