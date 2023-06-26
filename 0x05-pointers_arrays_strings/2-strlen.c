97e39f7946c34aa5ef24cce2bac042d6144a2ef5
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-06-26 11:44:50 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 
 /**
  * _strelen - returns the length of a string
- * @n: the string
+ * @s: the string
  * Return: the length of given string
  */
 
 int _strlen(char *s)
 {
-	int n;
+	int i;
 
 	i = 0;
 	while (s[i])
