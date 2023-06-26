60f344efc0e7aa52202ebaf6fd396d027216c86d
ElMehdi02 <bensoudamehdi6@gmail.com>
2023-06-26 03:41:24 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strelen - returns the length of a string
+ * @n: the string
+ * Return: the length of given string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
