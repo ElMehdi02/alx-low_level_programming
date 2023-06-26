4789283e065c1de038d30dda32cdafcdb51414ec
ElMehdi02 <bensoudamehdi6@gmail.com>
2023-06-26 04:12:37 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @str: the string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	 i = o;
+	 while ( str[i])
+	 {
+		 i++;
+	 }
+	 i--;
+	 while (i >= 0)
+	 {
+		 _putchar(str[i]);
+		 i--;
+	 }
+	 _putchar('\n');
+}
