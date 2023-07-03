e4da3da356247ac709fd34f3d3bd1dacd214f27a
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-03 13:38:15 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,10 +11,10 @@ void print_chessboard(char (*a)[8])
 	int j;
 
 	i = 0;
-	while (a[i] && j < 8)
+	while (a[i] && i < 8)
 	{
 		j = 0;
-		while (a[i][j] && j , 8)
+		while (a[i][j] && j < 8)
 		{
 			_putchar(a[i][j]);
 			j++;
