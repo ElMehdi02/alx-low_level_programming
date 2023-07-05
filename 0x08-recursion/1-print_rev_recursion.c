b7eec2485a064b81aa62b0434fb9bc177b473016
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-05 12:51:53 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,7 +3,7 @@
 
 /**
  * _print_rev_recursion - prints a given string in reverse
- * @s: the string
+ * @s: the string to be printed
  */
 
 
@@ -11,6 +11,6 @@ void _print_rev_recursion(char *s)
 {
 	if (!*s)
 		return;
-	print_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
