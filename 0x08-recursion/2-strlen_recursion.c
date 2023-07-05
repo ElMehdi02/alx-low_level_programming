18333501072601424524ff15b44837b001810743
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-05 13:09:03 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * -strlen_recursion - gets the len of a string using recursion
- *  @s: the string
- *  Return: the length of string s
+ * _strlen_recursion - gets the len of a string using recursion
+ * @s: the string
+ * Return: the length of string s
  */
 
 int _strlen_recursion(char *s)
