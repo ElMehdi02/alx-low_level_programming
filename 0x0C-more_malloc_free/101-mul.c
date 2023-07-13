08bcd4737ed201ab262ed23392a564d72ded1940
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-13 19:11:00 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,27 +3,27 @@
 
 int multiply(int num1, int num2)
 {
-	return (num1 * num2);
+return (num1 * num2);
 }
 
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	if (argc != 3)
-	{
-		printf("Error \n");
-		return (98);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+int num1, num2;
+if (argc != 3)
+{
+printf("Error \n");
+return (98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
 
-	if (num1 <= 0 || num2 <= 0)
-	{
-		printf("Error\n");
-		return (98);
-	}
+if (num1 <= 0 || num2 <= 0)
+{
+printf("Error\n");
+return (98);
+}
 
-	printf("%d\n", multiply(num1, num2));
+printf("%d\n", multiply(num1, num2));
 
-	return (0)
+return (0);
 }
