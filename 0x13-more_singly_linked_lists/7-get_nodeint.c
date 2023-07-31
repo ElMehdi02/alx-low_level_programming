e4fa0b0e03820fdb0498ece6dbe16df04763f6cc
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-31 19:22:33 +0100

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,13 +2,13 @@
 
 /**
  * get_nodeint_at_index - Returns the node at
- * the specofied index in a linked list
+ * the specified index in a linked list.
  *
- * @head: a pointer to the head of the linked list
- * @index: the index of the node to retrieve
+ * @head: A pointer to the head of the linked list.
+ * @index: The index of the node to retrieve.
  *
- * Return: a pointer to the node at the specified index
- * or NULL if index is out of range
+ * Return: A pointer to the node at the specified index
+ * or NULL if index is out of range.
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
