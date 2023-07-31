ff61c2ff81f0066080d1c9b9894064cfcf6a47d6
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-31 19:21:28 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list and sets the head pointer to NULL.
+ * free_listint - Frees a linked list and sets the head pointer to NULL.
  *
- * @head: a pointer to the head of the linked list.
+ * @head: A pointer to the head of the linked list.
  */
 
 void free_listint(listint_t *head)
