6bd73ab26c57f8a4720c6c654d5fcd8633a85dda
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-31 19:54:23 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a listint_t list
- * @h: pointer to the head of the list
+ * free_listint_safe - Frees a listint_t list.
+ * @h: Pointer to the head of the list.
  *
- * Return: the size of the list that was free'd
+ * Return: The size of the list that was free'd.
  */
 
 size_t free_listint_safe(listint_t **h)
