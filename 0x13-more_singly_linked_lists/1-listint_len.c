d084bc90a384ad618ac620eda7fb3d86d4f05ba2
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-31 19:20:12 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the number of elements in a linked list
- * @h: a pointer to the head of the linked list
- * Return: the number of elements in the linked list
+ *listint_len - Returns the number of elements in a linked list.
+ *@h: A pointer to the head of the linked list.
+ *Return: The number of elements in the linked list.
  */
 
-soze_t listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	int counter = 0;
 
