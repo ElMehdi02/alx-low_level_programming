75b620a5c73a50d45f4bded148503f8e4a6ee548
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-07-31 20:21:32 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _lists_h_
+#define _lists_h_
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -31,5 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LISTS_H */
+
+#endif
