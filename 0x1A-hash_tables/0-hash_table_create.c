f829c206ff3bd62e1ac95964b0d14e7055413589
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-10-05 11:34:36 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,15 +4,15 @@
  * hash_table_create - creates a hashtable
  *
  * @size: the size of the hash table
- * Return: hash_table created or NULL if something goes wong;
+ * Return: hash_table created or NULL if something goes wrong;
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 
-	hash_table = malloc(sizeoff(hash_table_t));
-	if(!hash_table)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (!hash_table)
 		return (NULL);
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
